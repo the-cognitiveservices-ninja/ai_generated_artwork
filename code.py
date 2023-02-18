6ed9239578c465fdb98c866e9f620b66223b8f6d
Holger Imbery <holger@imbery.de>
2023-02-18 16:24:34 +0000

    readme update and first run

--- code.py
+++ code.py
@@ -6,9 +6,11 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import os
 from datetime import datetime
+import sys
 
-# get API Key
-openai.api_key = os.environ['OPENAI_API_KEY']
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 
 def send_openai_request(engine, prompt, max_tokens=1000):
     response = openai.Completion.create(
@@ -22,18 +24,19 @@ def send_openai_request(engine, prompt, max_tokens=1000):
     return response
 
 
-# prompt to the language model
-prompt_base = "Generate a detailed Dall-E prompt with several adjectives for "  # an introduction text telling the language model what to do
-prompt_details = "two boys, two girls visible from behind sitting on the floor infront of brandenburg gate, drinking beer and smoking weed on a sunny day"
-additional_keywords = ", HD photograph, nikon camera, studio lighting, on Kodak TriX film"  # these keywords will be added after the language model generated the prompt. Example: "digital art", "oil painting", "water color painting", "high quality"
+prompt_base = "Generate a detailed Dall-E prompt with several adjectives for "
+prompt_details = sys.argv[1]
+additional_keywords = (
+    ", HD photograph, nikon camera, studio lighting, on Kodak TriX film"
+)
 
-model = "text-davinci-003"  # the version of the openai language model
-# generate a response
+model = "text-davinci-003"
+
 response = send_openai_request(model, prompt_base + prompt_details)
 
-# print the response from the language model
 generated_prompt = response["choices"][0]["text"]
-# define the request
+
+
 def send_openai_request(engine, prompt, max_tokens=1000):
     response = openai.Completion.create(
         engine=engine,
@@ -50,28 +53,25 @@ response = send_openai_request(model, prompt_base + prompt_details)
 
 
 generated_prompt = response["choices"][0]["text"]
-# image parameters
-number_of_images = 2  # how many images you want to generate
-image_size = "1024x1024"  # the size of the images
+number_of_images = 2
+image_size = "1024x1024"
 image_generation_prompt = f"{generated_prompt} {additional_keywords}"
 
-# define and send the request to dall-e with the generated prompt
 response = openai.Image.create(
     prompt=image_generation_prompt,
     n=number_of_images,
     size=image_size,
 )
 
-# set the timestamp for data processing
+
 timestamp_string = response.created
 datetime_string = datetime.fromtimestamp(timestamp_string).strftime("%Y%m%d%H%M%S")
 
-# get the image(s) from the response
+
 def get_images(response):
-    # generate an empty list for the image urls
+
     image_list = []
 
-    # store the image urls in the list
     for imgurl in response.data:
         image_list.append(imgurl.url)
     return image_list
@@ -79,22 +79,20 @@ def get_images(response):
 
 image_list = get_images(response)
 
-# display image urls
+
 print(image_list)
 
-# download the images
+
 def download_images(image_list, datetime_string):
-    # create a folder for the images
     os.mkdir(f"images/{datetime_string}")
 
-    # download the images
     for i in range(len(image_list)):
         urllib.request.urlretrieve(image_list[i], f"images/{datetime_string}/{i}.png")
 
 
 download_images(image_list, datetime_string)
 
-# display the images
+
 def display_images(image_list):
     for i in range(len(image_list)):
         img = mpimg.imread(f"images/{datetime_string}/{i}.png")
@@ -104,5 +102,4 @@ def display_images(image_list):
 
 display_images(image_list)
 
-# display the prompt
 print(image_generation_prompt)
